9eeac041e4eaee044d09448000c0f2c2f410df66
Hemant Bhanot <hemant.bhanot01@gmail.com>
2020-02-01 17:10:52 -0500

    refactored code

--- backend/speechToText.py
+++ backend/speechToText.py
@@ -3,12 +3,12 @@ import os
 
 # Imports the Google Cloud client library
 from google.cloud import speech
-def speechToText(uri):
+def speechToText():
     # Instantiates a client
     client = speech.SpeechClient()
 
     # The name of the audio file to transcribe
-    audio = speech.types.RecognitionAudio(uri)
+    audio = speech.types.RecognitionAudio(uri = 'gs://recording-for-speech/song.mp3')
     
     config = speech.types.RecognitionConfig(
             encoding=speech.enums.RecognitionConfig.AudioEncoding.LINEAR16,
@@ -17,4 +17,9 @@ def speechToText(uri):
 
     # Detects speech in the audio file
     response = client.recognize(config, audio)
-    return response.results.alternatives[0].transcript
+    print(response)
+    return response
+    #return response.results.alternatives[0].transcript
+
+if __name__ == "__main__":
+    speechToText()
