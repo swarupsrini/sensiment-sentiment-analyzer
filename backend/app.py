e12f45b6f422707fe62cb38fb5a078a0bd6cd81d
Jhong098 <Jhong098@gmail.com>
2020-02-01 15:59:52 -0500

    add CORS

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,20 @@
 from flask import Flask, request, jsonify
 from google.cloud import storage
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route("/getSentimentData", methods=['POST'])
 def get_sentiment_data():
     # save file
     data = request.get_data(cache=False)
+    print(request.get_data())
     print(data)
     with open("song.mp3", "wb") as song:
         song.write(data)
-    
+
     # upload to blob
     bucket_name = "recording-for-speech"
     source_file_name = "song.mp3"
@@ -20,7 +24,8 @@ def get_sentiment_data():
     blob = bucket.blob(destination_blob_name)
     blob.upload_from_filename(source_file_name)
 
-    # 
+    #
+
 
 if __name__ == "__main__":
     app.run()
--- frontend/src/Components/Upload/index.js
+++ frontend/src/Components/Upload/index.js
@@ -1,7 +1,20 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 
+const SERVER_URL = "http://localhost:5000";
+
 const Upload = () => {
+  const [receivedFiles, setReceivedFiles] = useState([]);
+
+  const sendToServer = file => {
+    fetch(`${SERVER_URL}/getSentimentData`, {
+      method: "POST",
+      body: file
+    })
+      .then(resp => resp.json())
+      .catch(error => console.log(error));
+  };
+
   const onDrop = useCallback(acceptedFiles => {
     acceptedFiles.forEach(file => {
       const reader = new FileReader();
@@ -12,11 +25,20 @@ const Upload = () => {
         // Do whatever you want with the file contents
         const binaryStr = reader.result;
         console.log(binaryStr);
+        sendToServer(file);
+        const fileInfo = (
+          <li key={file.path}>
+            {file.path} - {file.size} bytes
+          </li>
+        );
+        setReceivedFiles([...receivedFiles, fileInfo]);
       };
       reader.readAsArrayBuffer(file);
     });
   }, []);
+
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
   return (
     <div {...getRootProps()}>
       <input {...getInputProps()} />
@@ -25,6 +47,7 @@ const Upload = () => {
       ) : (
         <p>Drag 'n' drop some files here, or click to select files</p>
       )}
+      {receivedFiles}
     </div>
   );
 };
