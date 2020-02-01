d972e7fcb35bda3deef03582b187015dff058adf
joeyzhou98 <joeyzhou7002@gmail.com>
2020-02-01 15:39:47 -0500

    better setup for nlu

--- backend/sentiment.py
+++ backend/sentiment.py
@@ -3,9 +3,7 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson.natural_language_understanding_v1 import Features, EmotionOptions
 
 
-def analyze(input_text):
-
-    # Setup
+def setup():
     with open(".ibm-api-key", "r") as f:
         key = f.read()
     service_url = "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/57bf40d3-1788-445f-8717-63ab3b83442e"
@@ -16,7 +14,10 @@ def analyze(input_text):
     )
     natural_language_understanding.set_service_url(service_url=service_url)
 
-    # Analyze
+    return natural_language_understanding
+
+
+def analyze(natural_language_understanding, input_text):
     response = natural_language_understanding.analyze(
         text=input_text,
         features=Features(emotion=EmotionOptions())).get_result()
@@ -25,4 +26,5 @@ def analyze(input_text):
 
 
 if __name__ == "__main__":
-    print(analyze("Hi there, my name is Bob and I am sad"))
+    nlu = setup()
+    print(analyze(nlu, "Hi there, my name is Bob and I am sad"))
