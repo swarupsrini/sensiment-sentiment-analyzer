b4508321695ad727101232118101d09c6c4bbc3d
swarupsrini <swarup.srinivasan@mail.utoronto.ca>
2020-02-01 15:39:07 -0500

    finished uploading to blob storage in GCP

--- backend/app.py
+++ backend/app.py
@@ -11,4 +11,13 @@ def get_sentiment_data():
     with open("song.mp3", "wb") as song:
         song.write(data)
     
-    
+    # upload to blob
+    bucket_name = "recording-for-speech"
+    source_file_name = "song.mp3"
+    destination_blob_name = "song.mp3"
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(source_file_name)
+
+    # 
