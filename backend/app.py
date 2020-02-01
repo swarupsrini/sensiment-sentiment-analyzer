c2bf28c125d2d04baa4aa27b7be0f35f29f7f765
swarupsrini <swarup.srinivasan@mail.utoronto.ca>
2020-02-01 16:52:37 -0500

    fixes

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from google.cloud import storage
 from flask_cors import CORS
+import speechToText
 
 app = Flask(__name__)
 CORS(app)
@@ -10,8 +11,7 @@ CORS(app)
 def get_sentiment_data():
     # save file
     data = request.get_data(cache=False)
-    print(request.get_data())
-    print(data)
+    # print(data)
     with open("song.mp3", "wb") as song:
         song.write(data)
 
@@ -19,13 +19,15 @@ def get_sentiment_data():
     bucket_name = "recording-for-speech"
     source_file_name = "song.mp3"
     destination_blob_name = "song.mp3"
+
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
     blob.upload_from_filename(source_file_name)
 
-    #
-
+    # speech to text
+    text = speechToText.speechToText(f"gs://{bucket_name}/{destination_blob_name}")
+    print(text)
 
 if __name__ == "__main__":
     app.run()
