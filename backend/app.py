bf7a191addac0dd5b4ad6ef3d84583b6b356318c
swarupsrini <swarup.srinivasan@mail.utoronto.ca>
2020-02-01 18:54:34 -0500

    fixed recognition

--- backend/app.py
+++ backend/app.py
@@ -9,32 +9,10 @@ CORS(app)
 
 @app.route("/getSentimentData", methods=['POST'])
 def get_sentiment_data():
-    # save file
     data = request.get_data(cache=False)
-    # print(data)
-    # with open("song_temp.mp3", "wb") as song:
-    #     song.write(data)
-
-    # # upload to blob
-    # bucket_name = "recording-for-speech"
-    # source_file_name = "song_temp.mp3"
-    # destination_blob_name = "song.mp3"
-
-    # storage_client = storage.Client()
-    # bucket = storage_client.bucket(bucket_name)
-    # blob = bucket.blob(destination_blob_name)
-    # blob.upload_from_filename(source_file_name)
-
-    # speech to text
-    # res = speechToText.speechToText(f"gs://{bucket_name}/{destination_blob_name}")
     res = speechToText.speechToText(data)
-
-    print(len(res.results))
-    for i in range(len(res.results)):
-        print(type(res.results[i]))
-    # print(text.results)
-    # print(type(text.results))
-    # print(help(text.results))
+    print(res)
+    
 
 if __name__ == "__main__":
     app.run()
--- backend/speechToText.py
+++ backend/speechToText.py
@@ -1,34 +1,20 @@
 import io
 import os
-
-# Imports the Google Cloud client library
 from google.cloud import speech
+
 def speechToText(content):
-    # # Instantiates a client
     client = speech.SpeechClient()
-
-    # # The name of the audio file to transcribe
-    # audio = speech.types.RecognitionAudio(uri = uri)
-    
-    # config = speech.types.RecognitionConfig(
-    #         encoding=speech.enums.RecognitionConfig.AudioEncoding.LINEAR16,
-    #         language_code='en-US',
-    #         sample_rate_hertz=44100)
-
-    # # Detects speech in the audio file
-    # response = client.recognize(config, audio)
-    # # print(response)
-    # return response
-    # return response.results.alternatives[0].transcript
-
-    audio = speech.types.RecognitionAudio(content=content)
-    print(content)
+    audio = {"content": content}
     config = speech.types.RecognitionConfig(
         encoding=speech.enums.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=16000,
+        sample_rate_hertz=8000,
         language_code='en-US')
     response = client.recognize(config, audio)
-    return response
-
-if __name__ == "__main__":
-    speechToText()
+    res = ""
+    for result in response.results:
+        # First alternative is the most probable result
+        res += result.alternatives[0].transcript
+        # print(type(result))
+        # alternative = result.alternatives[0]
+        # print(u"Transcript: {}".format(alternative.transcript)) 
+    return res
