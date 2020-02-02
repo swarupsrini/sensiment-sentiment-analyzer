ad4ae644c88b8d18e2de6673a76efbc42c5e49c7
Hemant Bhanot <hemant.bhanot01@gmail.com>
2020-02-01 19:05:47 -0500

    called ibm api functions

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from google.cloud import storage
 from flask_cors import CORS
 import speechToText
+import sentiment
 
 app = Flask(__name__)
 CORS(app)
@@ -12,6 +13,8 @@ def get_sentiment_data():
     data = request.get_data(cache=False)
     res = speechToText.speechToText(data)
     print(res)
+    finRes = sentiment.analyze(sentiment.setup(), res)
+    print(finRes)
     
 
 if __name__ == "__main__":
