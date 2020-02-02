c7af33c2b36d529e6720cbcd88702ef771b76f54
swarupsrini <swarup.srinivasan@mail.utoronto.ca>
2020-02-02 10:10:31 -0500

    finished keyword relaying

--- backend/app.py
+++ backend/app.py
@@ -34,21 +34,22 @@ def get_sentiment_data_stream():
     amt = 3
     name = request.args.get('name')
     data = request.get_data(cache=False)
-    res = speechToText.speechToText(data)[0:50]
-    res = res.split()
+    ress = speechToText.speechToText(data)[0:50]
+    res = ress.split()
     splits = [" ".join(res[0:i]) for i in range(len(res)//amt, len(res)+len(res)//amt, len(res)//amt)]
+    data = sentiment.analyze(nlu, ress)
+    keys = [data["keywords"][i]["text"] for i in range(len(data["keywords"]))]
     for i in range(len(splits)):
-        splits[i] = sentiment.analyze(nlu, splits[i])[
-            "emotion"]["document"]["emotion"]
-    print(splits[0])
+        splits[i] = sentiment.analyze(nlu, splits[i])["emotion"]["document"]["emotion"]
     ret = {}
     ret["anger"] = [splits[i]["anger"] for i in range(len(splits))]
     ret["sadness"] = [splits[i]["sadness"] for i in range(len(splits))]
     ret["joy"] = [splits[i]["joy"] for i in range(len(splits))]
     ret["fear"] = [splits[i]["fear"] for i in range(len(splits))]
     ret["disgust"] = [splits[i]["disgust"] for i in range(len(splits))]
-    cache[name] = ret
-    return {"items": ret}
+    cache[name] = {"items": ret, "keywords": keys, "name": name}
+    print({"items": ret, "keywords": keys, "name": name})
+    return {"items": ret, "keywords": keys, "name": name}
 
 
 @app.route("/getSentimentCache", methods=["POST"])
