1d71c1f3c6a6921d1b456fffc52b5a4223865f61
swarupsrini <swarup.srinivasan@mail.utoronto.ca>
2020-02-02 10:40:27 -0500

    fixed formats

--- backend/app.py
+++ backend/app.py
@@ -34,22 +34,33 @@ def get_sentiment_data_stream():
     amt = 3
     name = request.args.get('name')
     data = request.get_data(cache=False)
-    ress = speechToText.speechToText(data)[0:50]
+    ress = speechToText.speechToText(data)
     res = ress.split()
     splits = [" ".join(res[0:i]) for i in range(len(res)//amt, len(res)+len(res)//amt, len(res)//amt)]
     data = sentiment.analyze(nlu, ress)
     keys = [data["keywords"][i]["text"] for i in range(len(data["keywords"]))]
     for i in range(len(splits)):
         splits[i] = sentiment.analyze(nlu, splits[i])["emotion"]["document"]["emotion"]
-    ret = {}
-    ret["anger"] = [splits[i]["anger"] for i in range(len(splits))]
-    ret["sadness"] = [splits[i]["sadness"] for i in range(len(splits))]
-    ret["joy"] = [splits[i]["joy"] for i in range(len(splits))]
-    ret["fear"] = [splits[i]["fear"] for i in range(len(splits))]
-    ret["disgust"] = [splits[i]["disgust"] for i in range(len(splits))]
-    cache[name] = {"items": ret, "keywords": keys, "name": name}
-    print({"items": ret, "keywords": keys, "name": name})
-    return {"items": ret, "keywords": keys, "name": name}
+
+    items = []
+    items.append({"id": "anger",
+                  "data": [{"x": i, "y": splits[i]["anger"]} for i in range(len(splits))]
+                })
+    items.append({"id": "sadness",
+                  "data": [{"x": i, "y": splits[i]["sadness"]} for i in range(len(splits))]
+                })
+    items.append({"id": "joy",
+                  "data": [{"x": i, "y": splits[i]["joy"]} for i in range(len(splits))]
+                })
+    items.append({"id": "fear",
+                  "data": [{"x": i, "y": splits[i]["fear"]} for i in range(len(splits))]
+                })
+    items.append({"id": "disgust",
+                  "data": [{"x": i, "y": splits[i]["disgust"]} for i in range(len(splits))]
+                })
+    ret = {"items": items, "keywords": keys, "name": name, "text": ress}
+    cache[name] = ret
+    return ret
 
 
 @app.route("/getSentimentCache", methods=["POST"])
